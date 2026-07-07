01da16902ef73cdab08bce4d2d7b65e7fd909f7b
Jaakko Haakana <jaakko@haakana.net>
2026-07-07 10:46:00 +0300

    Add missing files

---
+++ water2mqtt.Test/CalcTest.cs
@@ -0,0 +1,71 @@
+﻿using Xunit;
+
+namespace water2mqtt.Test
+{
+    public class CalcTest
+    {
+        [InlineData(68.69207947966561, 36.19724334064085, 285.7092535022176, 139.08814556691993, 0.3811)]
+        [InlineData(157.49275022981033, 42.20704861002906, 248.16691014364528, 239.00342997277914, 0.6714)]
+        [InlineData(299.1734212387239, 286.55152910244044, 97.97415595121322, 9.14779541899685, 0.0278)]
+        [InlineData(23.70795955845267,
+            133.8687651029021,
+            331.2015718939675,
+            144.76388159015403, 0.3930)]
+        /*[InlineData(168.029752290219,
+            93.49932798157528,
+            104.26502392300938,
+            8.954995268888638,
+            0.0278)]*/
+        [Theory]
+        public void Test(double a1, double a2, double a3, double a4, double expected)
+        {
+            /*
+
+trce: water2mqtt.ZennerMnk[0]
+                     0 Angle 3,3963181598967367
+               trce: water2mqtt.ZennerMnk[0]
+                     1 Angle 355,24290076270796
+               trce: water2mqtt.ZennerMnk[0]
+                     2 Angle 0,29842599180784646
+               trce: water2mqtt.ZennerMnk[0]
+                     3 Angle 250,56911489959396
+               trce: water2mqtt.ZennerMnk[0]
+                     Proposed decimals: 0,7100
+                            */
+            var decs = ZennerMnk.AnglesToDecimals(new double[] { a1, a2, a3, a4 });
+
+            Assert.Equal((decimal)expected, decs);
+        }
+    }
+}
+
+/*
+
+
+trce: water2mqtt.ZennerMnk[0]
+         0 Angle 168,029752290219
+   trce: water2mqtt.ZennerMnk[0]
+         1 Angle 93,49932798157528
+   trce: water2mqtt.ZennerMnk[0]
+         2 Angle 104,26502392300938
+   trce: water2mqtt.ZennerMnk[0]
+         3 Angle 8,954995268888638
+   trce: water2mqtt.ZennerMnk[0]
+         Proposed decimals: 0,0324
+   info: water2mqtt.ZennerMnk[0]
+         New good 1069,0324
+
+
+trce: water2mqtt.ZennerMnk[0]
+         0 Angle 299,1734212387239
+   trce: water2mqtt.ZennerMnk[0]
+         1 Angle 286,55152910244044
+   trce: water2mqtt.ZennerMnk[0]
+         2 Angle 97,97415595121322
+   trce: water2mqtt.ZennerMnk[0]
+         3 Angle 9,14779541899685
+   trce: water2mqtt.ZennerMnk[0]
+         Proposed decimals: 0,0278
+
+*/
+
---
+++ water2mqtt.Test/ErrorCorrectingProxyTest.cs
@@ -0,0 +1,204 @@
+﻿using System.Diagnostics;
+using System.Threading.Channels;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace water2mqtt.Test;
+
+public class ErrorCorrectingProxyTest
+{
+    private readonly ErrorCorrectingProxy proxy;
+    private Volume initial = Volume.FromCubicMeters(1024.2355m);
+    private DateTimeOffset initialTime = new (2025, 2, 9, 1, 2, 3, TimeSpan.Zero);
+    private Mock<IReadingStorage> storage = new();
+    private Mock<TimeProvider> time = new();
+    private BufferBlock<Volume> meterReadings = new();
+    private ITestOutputHelper output;
+
+    public ErrorCorrectingProxyTest(ITestOutputHelper output)
+    {
+        this.output = output;
+        var meter = new Mock<IWaterMeterRaw>();
+
+        SetupInitial(initial);
+
+        meter.Setup(m => m.GetNextValue(It.IsAny<CancellationToken>()))
+            .Returns((CancellationToken cancel) => meterReadings.ReceiveAsync(cancel));
+
+        time.Setup(t => t.GetUtcNow()).Returns(initialTime);
+
+        var log = output.BuildLoggerFor<ErrorCorrectingProxy>();
+
+        proxy = new ErrorCorrectingProxy(meter.Object, time.Object, log, storage.Object);
+    }
+
+    private TimeSpan DefaultTimeout => Debugger.IsAttached ? TimeSpan.FromDays(1) : TimeSpan.FromSeconds(5);
+
+    void SetupInitial(Volume init)
+    {
+        storage.Setup(s => s.LatestGood).Returns(init);
+        storage.Setup(s => s.LatestGoodTimestamp).Returns(initialTime);
+    }
+
+    [Fact]
+    public async Task CanBeStoppedAndStarted()
+    {
+        using var cancel = new CancellationTokenSource(DefaultTimeout);
+
+        await proxy.StartAsync(cancel.Token);
+        await proxy.StopAsync(cancel.Token);
+    }
+
+    [Fact]
+    public async Task ShouldGiveInitialValue()
+    {
+        using var cancel = new CancellationTokenSource(DefaultTimeout);
+
+        await proxy.StartAsync(cancel.Token);
+
+        var val = await proxy.GetNextValue(cancel.Token);
+
+        Assert.Equal(initial, val.Volume);
+
+        await proxy.StopAsync(cancel.Token);
+    }
+
+    [Theory]
+    [InlineData(1.0, 0.001)]
+    public async Task PositiveChangeReported(double start, double next)
+    {
+        using var cancel = new CancellationTokenSource(DefaultTimeout);
+
+        storage.Setup(s => s.LatestGood).Returns(Volume.FromCubicMeters((decimal)start));
+
+        await proxy.StartAsync(cancel.Token);
+
+        Assert.Equal((decimal)start, (await proxy.GetNextValue(cancel.Token)).Volume.ToCubicMeters());
+
+        AdvanceTime(TimeSpan.FromMinutes(1));
+        meterReadings.Post(Volume.FromCubicMeters((decimal)next));
+
+        Assert.Equal((decimal)(start+next), (await proxy.GetNextValue(cancel.Token)).Volume.ToCubicMeters());
+
+        await proxy.StopAsync(cancel.Token);
+    }
+
+    [Theory]
+    [InlineData(1.0, 0.000, 0)]
+    public async Task ShouldReportFlowrate(double start, double next, double LperMin)
+    {
+        using var cancel = new CancellationTokenSource(DefaultTimeout);
+
+        storage.Setup(s => s.LatestGood).Returns(Volume.FromCubicMeters((decimal)start));
+
+        await proxy.StartAsync(cancel.Token);
+
+        Assert.Equal((decimal)start, (await proxy.GetNextValue(cancel.Token)).Volume.ToCubicMeters());
+        AdvanceTime(TimeSpan.FromMinutes(1));
+        meterReadings.Post(Volume.FromCubicMeters((decimal)start));
+
+        Assert.Equal((decimal)start, (await proxy.GetNextValue(cancel.Token)).Volume.ToCubicMeters());
+        AdvanceTime(TimeSpan.FromMinutes(1));
+        meterReadings.Post(Volume.FromCubicMeters((decimal)start));
+        Assert.Equal((decimal)start, (await proxy.GetNextValue(cancel.Token)).Volume.ToCubicMeters());
+        AdvanceTime(TimeSpan.FromMinutes(1));
+        meterReadings.Post(Volume.FromCubicMeters((decimal)start));
+        Assert.Equal((decimal)start, (await proxy.GetNextValue(cancel.Token)).Volume.ToCubicMeters());
+
+        AdvanceTime(TimeSpan.FromMinutes(1));
+        meterReadings.Post(Volume.FromCubicMeters((decimal)next));
+
+        var nextValue = await proxy.GetNextValue(cancel.Token);
+
+        Assert.Equal((decimal)(start+next), nextValue.Volume.ToCubicMeters());
+        Assert.Equal((decimal)LperMin, nextValue.FlowRate!.ToLitersPerMinute());
+
+        await proxy.StopAsync(cancel.Token);
+    }
+
+    void AdvanceTime(TimeSpan bySpan)
+    {
+        var now = time.Object.GetUtcNow();
+        time.Setup(t => t.GetUtcNow()).Returns(now + bySpan);
+    }
+
+    [Theory]
+    [InlineData(1.0, 0.9, 0.001)]
+    public async Task TooLargeChangeNotReported(double start, double ignored, double nextGood)
+    {
+        using var cancel = new CancellationTokenSource(DefaultTimeout);
+
+        storage.Setup(s => s.LatestGood).Returns(Volume.FromCubicMeters((decimal)start));
+
+        await proxy.StartAsync(cancel.Token);
+
+        Assert.Equal((decimal)start, (await proxy.GetNextValue(cancel.Token)).Volume.ToCubicMeters());
+
+        AdvanceTime(TimeSpan.FromMinutes(10));
+
+        meterReadings.Post(Volume.FromCubicMeters((decimal)ignored));
+        meterReadings.Post(Volume.FromCubicMeters((decimal)nextGood));
+
+        Assert.Equal((decimal)start, (await proxy.GetNextValue(cancel.Token)).Volume.ToCubicMeters());
+        Assert.Equal((decimal)1.001, (await proxy.GetNextValue(cancel.Token)).Volume.ToCubicMeters());
+
+        await proxy.StopAsync(cancel.Token);
+    }
+
+    async Task AssertNext(double expected, CancellationToken cancel)
+    {
+        Assert.Equal((decimal)expected, (await proxy.GetNextValue(cancel)).Volume.ToCubicMeters());
+    }
+
+    [Theory]
+    [InlineData(0.9990, 0.0001, 1.0001 )]
+    public async Task ShouldRollOverCubic(double start, double nextGood, double expected)
+    {
+        using var cancel = new CancellationTokenSource(DefaultTimeout);
+
+        storage.Setup(s => s.LatestGood).Returns(Volume.FromCubicMeters((decimal)start));
+
+        await proxy.StartAsync(cancel.Token);
+
+        await AssertNext(start, cancel.Token);
+
+        AdvanceTime(TimeSpan.FromMinutes(10));
+
+        meterReadings.Post(Volume.FromCubicMeters((decimal)nextGood));
+
+        await AssertNext(expected, cancel.Token);
+
+        await proxy.StopAsync(cancel.Token);
+    }
+
+    [Theory]
+    [InlineData(0.9990,
+        new[] { 0.010, 0.020, 0.1000, 0.2000 },
+        new[] { 1.010, 1.020, 1.1000, 1.2000 })]
+    public async Task SequenceOfValues(double start, double[] nextGoods, double[] expecteds)
+    {
+        using var cancel = new CancellationTokenSource(DefaultTimeout);
+
+        storage.Setup(s => s.LatestGood).Returns(Volume.FromCubicMeters((decimal)start));
+
+        await proxy.StartAsync(cancel.Token);
+
+        await AssertNext(start, cancel.Token);
+
+        for (var i = 0; i < nextGoods.Length; i++)
+        {
+            AdvanceTime(TimeSpan.FromMinutes(60));
+
+            var nextGood = nextGoods[i];
+            var expected = expecteds[i];
+            
+            meterReadings.Post(Volume.FromCubicMeters((decimal)nextGood));
+            await AssertNext(expected, cancel.Token);
+        }
+
+        await proxy.StopAsync(cancel.Token);
+    }
+}
